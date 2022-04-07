be8a27c60e3c2dee3be4a3d077df22e25fe88a97
ssssksss <ssssksss@gmail.com>
2022-04-07 14:51:10 +0900

    jenkins test

--- src/main/java/com/example/ssssksss_blog/SsssksssBlogApplication.java
+++ src/main/java/com/example/ssssksss_blog/SsssksssBlogApplication.java
@@ -3,6 +3,7 @@ package com.example.ssssksss_blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@SpringBootApplication
@@ -16,15 +17,20 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //
 @SpringBootApplication
-public class SsssksssBlogApplication {
+public class SsssksssBlogApplication extends SpringBootServletInitializer {
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 //			+ "classpath:application.yml,"
-			+ "/usr/local/tomcat8/application.yml";
-//			+ "C:/test/application.yml";
+//			+ "/usr/local/tomcat8/application.yml";
+			+ "C:/test/application.yml";
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(SsssksssBlogApplication.class)
 				.properties(APPLICATION_LOCATIONS)
 				.run(args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
 	}
 }
 
--- src/main/java/com/example/ssssksss_blog/config/WebMvcConfig.java
+++ src/main/java/com/example/ssssksss_blog/config/WebMvcConfig.java
@@ -10,6 +10,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://blog.ssssksss.xyz","http://localhost:3000","http://localhost:3002"
-                ,"https://ssssksss-blog-9ivlxt7ah-ssssksss.vercel.app/");
+                ,"https://ssssksss-blog-9ivlxt7ah-ssssksss.vercel.app");
     }
 }
