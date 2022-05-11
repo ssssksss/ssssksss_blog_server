75c5e98b1e8c687257e5c74d711796ed3fb4f0d0
ssssksss <ssssksss@gmail.com>
2022-05-12 03:09:57 +0900

    test

--- src/main/java/com/example/ssssksss_blog/config/SwaggerConfig.java
+++ src/main/java/com/example/ssssksss_blog/config/SwaggerConfig.java
@@ -17,25 +17,25 @@ import springfox.documentation.spring.web.plugins.Docket;
  * Description :
  */
 //http://localhost:8080/swagger-ui/index.html
-@Configuration
+//@Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.ssssksss_blog.blog.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Practice Swagger")
-                .description("practice swagger config")
-                .version("1.0")
-                .build();
-    }
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.OAS_30)
+//                .useDefaultResponseMessages(false)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.example.ssssksss_blog.blog.controller"))
+//                .paths(PathSelectors.any())
+//                .build()
+//                .apiInfo(apiInfo());
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("Practice Swagger")
+//                .description("practice swagger config")
+//                .version("1.0")
+//                .build();
+//    }
 }
